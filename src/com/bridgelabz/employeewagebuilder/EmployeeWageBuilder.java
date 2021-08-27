05649eba07dba1982af9640a1f4a9880778146c9
padmini v <padmini.v@perfios.com>
2021-08-27 20:46:26 +0530

    ADDED: condition of total

--- src/com/bridgelabz/employeewagebuilder/EmployeeWageBuilder.java
+++ src/com/bridgelabz/employeewagebuilder/EmployeeWageBuilder.java
@@ -7,13 +7,15 @@ public class EmployeeWageBuilder
 	public static final int IS_FULL_TIME=2;
     public static final int EMP_RATE_PER_HOUR=20;
     public static final int NUM_OF_WORKING_DAYS=2;
+    public static final int MAX_HRS_IN_MONTH=10;
     
 	public static void main(String[] args) 
 	{
 		
 		int empHrs=0;
-		int empWage=0, totalEmpWage=0;
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++){
+		int totalWorkingDays=0, totalEmpHrs=0;
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+			totalWorkingDays++;
 			int empCheck=(int)Math.floor(Math.random()*10)%3;
 			switch(empCheck){
 				case IS_PART_TIME:
@@ -25,12 +27,10 @@ public class EmployeeWageBuilder
 				default:
 					empHrs=0;
 			}	
-			empWage=empHrs*EMP_RATE_PER_HOUR;
-			totalEmpWage+=empWage;
-			System.out.println("EmpWage "+ empWage);
+			totalEmpHrs+=empHrs;
+			System.out.println("Day#:"+totalWorkingDays+"Emp Hr:"+empHrs);
       }
-      
-      System.out.println("Total EmpWage "+ totalEmpWage);
+      int totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOUR;
+      System.out.println("Total Emp Wage "+ totalEmpWage);
 	}
-
 }
